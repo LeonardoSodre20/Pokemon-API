a024fff2873090f9fc6ba923c581958f41e7a547
Leonardo Sodré <LeonardoSodre25@outlook.com>
2022-02-16 21:52:20 -0300

    em andamento

--- javascript/addPokemons.js
+++ javascript/addPokemons.js
@@ -1,13 +1,17 @@
 const containerAddPokemon = document.querySelector('[data-container-form="form"]')
 const buttonOpenForm = document.querySelector('[data-add-pokemon="pokemon"]')
-console.log(containerAddPokemon)
-
 const form = document.querySelector('[data-form="form"]')
+const buttonOfForm = document.querySelector('[data-btn-form="btnForm"]')
+
 
 const openFormClick = () => {
 
     containerAddPokemon.classList.toggle('active-form')
 }
+
+buttonOfForm.addEventListener('click', event => {
+    event.preventDefault()
+})
 
 buttonOpenForm.addEventListener('click', () => openFormClick())
 
